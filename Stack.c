e3334ef1f0e36595b2cb1c2a8e5f24054b9c8783
StarP1t <1067992446@qq.com>
2026-09-14 20:14:44 +0800

    更新：栈实现

--- Stack.c
+++ Stack.c
@@ -1,10 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_SIZE 100
 typedef int ElemType;
 
-typedef struct stack
+// 栈
+typedef struct
 {
-    ElemType data[MAX_SIZE];
+    ElemType* data;
     int top;
-} stack;
+} Stack;
 
+// 动态初始化
+Stack* init_stack()
+{
+    Stack* s = (Stack*)malloc(sizeof(Stack));
+    s->data = (ElemType*)malloc(sizeof(ElemType) * MAX_SIZE);
+    s->top = -1;
+    return s;
+}
+
+// 判断空栈
+int is_empty(Stack* s)
+{
+    return s->top == -1 ? 1 : 0;
+}
+
+// 压栈
+int push(Stack* s, ElemType e)
+{
+    if (s->top >= MAX_SIZE - 1)
+    {
+        printf("Stack full!\n");
+        return -1;
+    }
+
+    s->data[++s->top] = e;
+    return 0;
+}
+
+// 出栈
+int pop(Stack* s, ElemType *e)
+{
+    if (s->top == -1)
+    {
+        printf("Stack empty!\n");
+        return -1;
+    }
+
+    *e = s->data[s->top--];
+    return 0;
+}
+
+// 获取栈顶元素
+int get_top(Stack* s, ElemType *e)
+{
+    if (s->top == -1)
+    {
+        printf("Stack empty!\n");
+        return -1;
+    }
+
+    *e = s->data[s->top];
+    return 0;
+}
